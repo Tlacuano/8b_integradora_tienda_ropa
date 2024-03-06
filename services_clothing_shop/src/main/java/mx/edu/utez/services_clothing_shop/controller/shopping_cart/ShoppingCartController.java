10f8dd8f987bcb1890a7665bc1f9a4f69ad2d3dd
Kellguzdi <20213tn114@utez.edu.mx>
2024-03-06 01:55:24 -0600

    [ADD] ShoppingCart - postAndDelete :
    Implemented method to post and delete  shopping cart

---
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/shopping_cart/ShoppingCartController.java
@@ -0,0 +1,44 @@
+package mx.edu.utez.services_clothing_shop.controller.shopping_cart;
+import mx.edu.utez.services_clothing_shop.model.shopping_cart.BeanShopingCart;
+import mx.edu.utez.services_clothing_shop.service.sopphing_cart.SopphingCartServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/shopping-cart")
+public class ShoppingCartController {
+    @Autowired
+    private SopphingCartServices shoppingCartServices;
+
+    @GetMapping("/get-shopping-cart/{userEmail}")
+    public ResponseEntity<List<BeanShopingCart>> findShoppingCarsByUserEmail(@PathVariable String userEmail) {
+        List<BeanShopingCart> shoppingCarts = shoppingCartServices.findShoppingCarsByUserEmail(userEmail);
+        if (shoppingCarts.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(shoppingCarts);
+        }
+    }
+    @PostMapping("/save-new-shopping-cart")
+    public ResponseEntity<BeanShopingCart> createShoppingCar(@RequestBody BeanShopingCart shoppingCart) {
+        BeanShopingCart savedShoppingCart = shoppingCartServices.saveShoppingCar(shoppingCart);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedShoppingCart);
+    }
+    @DeleteMapping("/delete-shopping-cart/{shoppingCartId}")
+    public ResponseEntity<Void> deleteShoppingCartById(@PathVariable UUID shoppingCartId) {
+        try {
+            shoppingCartServices.deleteShoppingCartById(shoppingCartId);
+            return ResponseEntity.noContent().build();
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
---
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/shopping_cart/IShoppingCart.java
@@ -0,0 +1,12 @@
+package mx.edu.utez.services_clothing_shop.model.shopping_cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface IShoppingCart extends JpaRepository<BeanShopingCart,UUID> {
+    List<BeanShopingCart> findByUserEmail(String userEmail);
+}
