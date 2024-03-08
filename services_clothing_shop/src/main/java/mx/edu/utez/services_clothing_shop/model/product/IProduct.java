d45fddf60fdf5292b8fac7e3d9e05a6be2faff30
FREECS33 <20213tn116@utez.edu.mx>
2024-03-08 10:39:44 -0600

    [ADD] Interfaces, Services, Controllers - Product
    Added IProduct, ProductService, and ProductController with all its methods

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product/IProduct.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product/IProduct.java
@@ -1,10 +1,17 @@
 package mx.edu.utez.services_clothing_shop.model.product;
 
+import mx.edu.utez.services_clothing_shop.model.user.BeanUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IProduct extends JpaRepository<BeanProduct, UUID> {
+    List<BeanProduct> findByUser(BeanUser user);
+
+    boolean existsByIdProduct(UUID idProduct);
+
+    BeanProduct findByIdProduct(UUID idProduct);
 }
--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/product/ProductService.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/product/ProductService.java
@@ -1,25 +1,26 @@
 package mx.edu.utez.services_clothing_shop.service.product;
 
-import mx.edu.utez.services_clothing_shop.model.category.BeanCategory;
 import mx.edu.utez.services_clothing_shop.model.product.BeanProduct;
 import mx.edu.utez.services_clothing_shop.model.product.IProduct;
-import mx.edu.utez.services_clothing_shop.model.status.BeanStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import mx.edu.utez.services_clothing_shop.model.user.BeanUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductService {
+    private final IProduct iProduct;
 
-    @Autowired
-    private IProduct iProduct;
+    public ProductService(IProduct iProduct) {
+        this.iProduct = iProduct;
+    }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<List<BeanProduct>> findAll() {
+    public ResponseEntity<List<BeanProduct>> getProducts() {
         try {
             List<BeanProduct> products = iProduct.findAll();
             return ResponseEntity.ok(products);
@@ -28,8 +29,28 @@ public class ProductService {
         }
     }
 
+    @Transactional(readOnly = true)
+    public ResponseEntity<List<BeanProduct>> getProductsByUser(BeanUser user) {
+        try {
+            List<BeanProduct> products = iProduct.findByUser(user);
+            return ResponseEntity.ok(products);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<BeanProduct> getProduct(UUID idProduct) {
+        try {
+            BeanProduct product = iProduct.findById(idProduct).orElse(null);
+            return ResponseEntity.ok(product);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<BeanProduct> insert(BeanProduct product) {
+    public ResponseEntity<BeanProduct> postProduct(BeanProduct product) {
         try {
             return ResponseEntity.status(201).body(iProduct.save(product));
         } catch (Exception e) {
@@ -38,10 +59,10 @@ public class ProductService {
     }
 
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<BeanProduct> update(BeanProduct product) {
+    public ResponseEntity<BeanProduct> putProduct(BeanProduct product) {
         try {
-            if (iCategory.existsByCategory(category.getCategory())) {
-                return ResponseEntity.status(201).body(iCategory.save(category));
+            if (iProduct.existsByIdProduct(product.getIdProduct())) {
+                return ResponseEntity.status(201).body(iProduct.save(product));
             } else {
                 return ResponseEntity.status(400).build();
             }
@@ -51,19 +72,13 @@ public class ProductService {
     }
 
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<Boolean> updateStatus(String category) {
+    public ResponseEntity<Boolean> putStatusProduct(UUID idProduct) {
         try {
-            if (iCategory.existsByCategory(category)) {
-                BeanCategory beanCategory = iCategory.findByCategory(category);
-                BeanStatus status = beanCategory.getStatus();
-                if (status.getStatus().equals("ACTIVE")) {
-                    status.setStatus("INACTIVE");
-                } else {
-                    status.setStatus("ACTIVE");
-                }
-                beanCategory.setStatus(status);
-                iCategory.save(beanCategory);
-                return ResponseEntity.status(201).body(true);
+            if (iProduct.existsByIdProduct(idProduct)) {
+                BeanProduct product = iProduct.findByIdProduct(idProduct);
+                product.setStatus(!product.isStatus());
+                iProduct.save(product);
+                return ResponseEntity.status(200).body(true);
             } else {
                 return ResponseEntity.status(400).body(false);
             }
