97010d58efeac2e0d53644fb075552c5407753a4
DMarjar <20213tn119@utez.edu.mx>
2024-03-07 21:52:35 -0600

    [UPDATE] Payment cards - ResponseEntity: added response entity to the methods

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/payment_card/PaymentCardController.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/payment_card/PaymentCardController.java
@@ -5,29 +5,31 @@ import mx.edu.utez.services_clothing_shop.controller.payment_card.dto.RequestPay
 import mx.edu.utez.services_clothing_shop.controller.payment_card.dto.RequestPaymentCardDTO;
 import mx.edu.utez.services_clothing_shop.controller.payment_card.dto.ResponsePaymentCardDTO;
 import mx.edu.utez.services_clothing_shop.model.payment_card.BeanPaymentCard;
-import mx.edu.utez.services_clothing_shop.model.person.BeanPerson;
 import mx.edu.utez.services_clothing_shop.model.status.BeanStatus;
 import mx.edu.utez.services_clothing_shop.model.user.BeanUser;
 import mx.edu.utez.services_clothing_shop.service.payment_card.PaymentCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/payment-card")
+@RequestMapping("venta-ropa/api/payment-card")
 @CrossOrigin(origins = "*")
 public class PaymentCardController {
     @Autowired
     private PaymentCardService paymentCardService;
 
     @PostMapping("/payment-card-by-user-email")
-    public Page<ResponsePaymentCardDTO> getPaymentCardByUserEmail(@Valid @RequestBody RequestPaymentCardByUserEmailDTO requestBody) {
+    public ResponseEntity<Page<ResponsePaymentCardDTO>> getPaymentCardByUserEmail(@Valid @RequestBody RequestPaymentCardByUserEmailDTO requestBody) {
         Page<BeanPaymentCard> paymentCards = paymentCardService.getPaymentCardByUserEmail(requestBody.getEmail(), requestBody.getPage());
-        return paymentCards.map(paymentCard -> new ResponsePaymentCardDTO().toPaymentCardDTO(paymentCard));
+        Page<ResponsePaymentCardDTO> dtoPage = paymentCards.map(paymentCard -> new ResponsePaymentCardDTO().toPaymentCardDTO(paymentCard));
+        return new ResponseEntity<>(dtoPage, HttpStatus.OK);
     }
 
     @PostMapping("/save-payment-card")
-    public BeanPaymentCard savePaymentCard(@Valid @RequestBody RequestPaymentCardDTO paymentCard) {
+    public ResponseEntity<BeanPaymentCard> savePaymentCard(@Valid @RequestBody RequestPaymentCardDTO paymentCard) {
         BeanPaymentCard beanPaymentCard = new BeanPaymentCard();
         beanPaymentCard.setIdPaymentCard(paymentCard.getIdPaymentCard());
         beanPaymentCard.setCardholderName(paymentCard.getCardholderName());
@@ -43,6 +45,6 @@ public class PaymentCardController {
         user.setIdUser(paymentCard.getIdUser());
         beanPaymentCard.setUser(user);
 
-        return paymentCardService.savePaymentCard(beanPaymentCard);
+        return new ResponseEntity<>(paymentCardService.savePaymentCard(beanPaymentCard), HttpStatus.OK);
     }
 }
