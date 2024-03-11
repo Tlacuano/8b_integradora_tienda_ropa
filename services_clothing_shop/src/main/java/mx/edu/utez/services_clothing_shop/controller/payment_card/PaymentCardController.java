b8cfb068e021e6664b4963be02fd668d294bfb8f
DMarjar <20213tn119@utez.edu.mx>
2024-03-11 16:51:38 -0600

    [UPDATE] Payment cards - postPaymentCard: encryption methods.
    Some work still needs to be done in the response.

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/payment_card/PaymentCardController.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/payment_card/PaymentCardController.java
@@ -1,13 +1,13 @@
 package mx.edu.utez.services_clothing_shop.controller.payment_card;
 
 import jakarta.validation.Valid;
-import lombok.extern.java.Log;
 import mx.edu.utez.services_clothing_shop.controller.payment_card.dto.*;
 import mx.edu.utez.services_clothing_shop.model.card_status.BeanCardStatus;
 import mx.edu.utez.services_clothing_shop.model.payment_card.BeanPaymentCard;
 import mx.edu.utez.services_clothing_shop.model.user.BeanUser;
 import mx.edu.utez.services_clothing_shop.service.payment_card.PaymentCardService;
 import mx.edu.utez.services_clothing_shop.utils.CustomResponse;
+import mx.edu.utez.services_clothing_shop.utils.security.EncryptionFunctions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("venta-ropa/api/payment-card")
@@ -32,16 +31,16 @@ public class PaymentCardController {
     }
 
     @PostMapping("/post-payment-card")
-    public ResponseEntity<CustomResponse<RequestPaymentCardDTO>> postPaymentCard(@Valid @RequestBody RequestPaymentCardDTO paymentCard) {
+    public ResponseEntity<CustomResponse<ResponsePaymentCardDTO>> postPaymentCard(@Valid @RequestBody RequestPaymentCardDTO paymentCard) {
 
         if (paymentCardService.cardIsRegistered(paymentCard.getCardNumber(), paymentCard.getIdUser())) {
             throw new RuntimeException("payment.card.registered");
         }
 
         BeanPaymentCard beanPaymentCard = getBeanPaymentCard(paymentCard);
-        paymentCard.setIdPaymentCard(paymentCardService.postPaymentCard(beanPaymentCard).getIdPaymentCard());
+        ResponsePaymentCardDTO responsePaymentCard = new ResponsePaymentCardDTO().toPaymentCardDTO(paymentCardService.postPaymentCard(beanPaymentCard));
 
-        return new ResponseEntity<>(new CustomResponse<>(paymentCard, "Tarjeta de crédito registrada correctamente", false, 200), HttpStatus.OK);
+        return new ResponseEntity<>(new CustomResponse<>(responsePaymentCard, "Tarjeta de crédito registrada correctamente", false, 200), HttpStatus.OK);
     }
 
     @PostMapping("/put-payment-card-status")
@@ -78,10 +77,10 @@ public class PaymentCardController {
 
     private static BeanPaymentCard getBeanPaymentCard(RequestPaymentCardDTO paymentCard) {
         BeanPaymentCard beanPaymentCard = new BeanPaymentCard();
-        beanPaymentCard.setCardholderName(paymentCard.getCardholderName());
-        beanPaymentCard.setCardNumber(paymentCard.getCardNumber());
-        beanPaymentCard.setExpirationDate(paymentCard.getExpirationDate());
-        beanPaymentCard.setCvv(paymentCard.getCvv());
+        beanPaymentCard.setCardholderName(EncryptionFunctions.encryptString(paymentCard.getCardholderName()));
+        beanPaymentCard.setCardNumber(EncryptionFunctions.encryptString(paymentCard.getCardNumber()));
+        beanPaymentCard.setExpirationDate(EncryptionFunctions.encryptString(paymentCard.getExpirationDate()));
+        beanPaymentCard.setCvv(EncryptionFunctions.encryptString(paymentCard.getCvv()));
 
         BeanCardStatus status = new BeanCardStatus();
         status.setIdStatus(paymentCard.getIdStatus());
