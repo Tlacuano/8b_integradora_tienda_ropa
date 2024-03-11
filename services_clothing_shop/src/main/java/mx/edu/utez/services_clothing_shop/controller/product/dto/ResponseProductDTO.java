b705c52ff0f23cd5dd618750e39eff1ee7f8e2f9
FREECS33 <20213tn116@utez.edu.mx>
2024-03-11 11:28:49 -0600

    [UPDATE] Products - ResponseProductDTO
    Updated ResponseProductDTO file

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/product/dto/ResponseProductDTO.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/product/dto/ResponseProductDTO.java
@@ -2,8 +2,9 @@ package mx.edu.utez.services_clothing_shop.controller.product.dto;
 
 import lombok.Data;
 import mx.edu.utez.services_clothing_shop.model.product.BeanProduct;
-import mx.edu.utez.services_clothing_shop.model.subcategory.BeanSubcategory;
+import mx.edu.utez.services_clothing_shop.model.product_gallery.BeanProductGallery;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -13,21 +14,27 @@ public class ResponseProductDTO {
     private String description;
     private double price;
     private int amount;
-    private BeanSubcategory subcategory;
+    private String subcategory;
+    private String category;
     private boolean status;
+    private List<String> productGallery;
 
     public ResponseProductDTO() {
     }
 
-    public ResponseProductDTO toProductDTO(BeanProduct product) {
+    public static ResponseProductDTO toProductDTO(BeanProduct product) {
         ResponseProductDTO dto = new ResponseProductDTO();
         dto.setIdProduct(product.getIdProduct());
         dto.setProductName(product.getProductName());
         dto.setDescription(product.getDescription());
         dto.setPrice(product.getPrice());
         dto.setAmount(product.getAmount());
-        dto.setSubcategory(product.getSubcategory());
+        dto.setSubcategory(product.getSubcategory().getSubcategory());
+        dto.setCategory(product.getSubcategory().getCategory().getCategory());
         dto.setStatus(product.isStatus());
+        for (BeanProductGallery gallery : product.getProductGallery()) {
+            dto.getProductGallery().add(gallery.getImage());
+        }
         return dto;
     }
 }
