c7b7e5e3a6fcea555aac7c2e7269e19dfe76d5e6
FREECS33 <20213tn116@utez.edu.mx>
2024-03-12 16:55:52 -0600

    [ADD] Products - IProductGallery: Implements the interface for use the store procedure for insert the gallery of the product inserted

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product_gallery/IProductGallery.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product_gallery/IProductGallery.java
@@ -1,10 +1,13 @@
 package mx.edu.utez.services_clothing_shop.model.product_gallery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface IProductGallery extends JpaRepository<BeanProductGallery, UUID> {
+    @Query(value = "CALL sp_post_product_gallery(:idProduct, :idStatus: :image)", nativeQuery = true)
+    BeanProductGallery postProductGallery(String idProduct, String idStatus, String image);
 }
