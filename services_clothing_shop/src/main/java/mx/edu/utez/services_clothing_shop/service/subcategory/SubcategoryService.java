3d3052e3d57a93ced0164a01c09a2ebf1312f620
FREECS33 <20213tn116@utez.edu.mx>
2024-03-12 11:16:16 -0600

    [UPDATE] Subcategories - SubcategoryService: Updated all methods in SubcategoryService to correctly utilize pagination and perform basic services

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/subcategory/SubcategoryService.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/subcategory/SubcategoryService.java
@@ -2,12 +2,12 @@ package mx.edu.utez.services_clothing_shop.service.subcategory;
 
 import mx.edu.utez.services_clothing_shop.model.subcategory.BeanSubcategory;
 import mx.edu.utez.services_clothing_shop.model.subcategory.ISubCategory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -18,63 +18,33 @@ public class SubcategoryService {
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<List<BeanSubcategory>> getSubcategories() {
-        try {
-            List<BeanSubcategory> subcategories = iSubCategory.findAll();
-            return ResponseEntity.ok(subcategories);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public Page<BeanSubcategory> getSubcategories(Pageable page) {
+        return iSubCategory.findAll(page);
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<BeanSubcategory> getSubcategory(UUID idSubcategory) {
-        try {
-            if (iSubCategory.existsByIdSubcategory(idSubcategory)) {
-                return ResponseEntity.ok(iSubCategory.findByIdSubcategory(idSubcategory));
-            } else {
-                return ResponseEntity.status(400).build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public BeanSubcategory getSubcategory(UUID idSubcategory) {
+        return iSubCategory.findByIdSubcategory(idSubcategory);
     }
 
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<BeanSubcategory> postSubcategory(BeanSubcategory subcategory) {
-        try {
-            return ResponseEntity.status(201).body(iSubCategory.save(subcategory));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public BeanSubcategory postSubcategory(BeanSubcategory subcategory) {
+        return iSubCategory.save(subcategory);
     }
 
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<BeanSubcategory> putSubcategory(BeanSubcategory subcategory) {
-        try {
-            if (iSubCategory.existsByIdSubcategory(subcategory.getIdSubcategory())) {
-                return ResponseEntity.status(201).body(iSubCategory.save(subcategory));
-            } else {
-                return ResponseEntity.status(400).build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public BeanSubcategory putSubcategory(BeanSubcategory subcategory) {
+        return iSubCategory.save(subcategory);
     }
 
     @Transactional(rollbackFor = {SQLException.class})
-    public ResponseEntity<Boolean> putStatusSubcategory(BeanSubcategory subcategory) {
-        try {
-            if (iSubCategory.existsByIdSubcategory(subcategory.getIdSubcategory())) {
-                BeanSubcategory beanSubcategory = iSubCategory.findByIdSubcategory(subcategory.getIdSubcategory());
-                beanSubcategory.setStatus(!beanSubcategory.isStatus());
-                iSubCategory.save(beanSubcategory);
-                return ResponseEntity.status(200).body(true);
-            } else {
-                return ResponseEntity.status(400).body(false);
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+    public Boolean putStatusSubcategory(BeanSubcategory subcategory) {
+        BeanSubcategory subcategory1 = iSubCategory.findByIdSubcategory(subcategory.getIdSubcategory());
+        if (subcategory1 != null) {
+            subcategory1.setStatus(!subcategory1.isStatus());
+            iSubCategory.save(subcategory1);
+            return true;
         }
+        return false;
     }
 }
