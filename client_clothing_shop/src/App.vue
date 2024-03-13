ba935346c0057baff0a34a37a9e868434e329557
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-03-13 14:51:47 -0600

    [ADD] front-end - NavbarAdmin : to begin make the components of the aplication

--- client_clothing_shop/src/App.vue
+++ client_clothing_shop/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
+    <NavbarAdmin />
     <b-container fluid>
       <router-view />
     </b-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    NavbarAdmin: () => import('./components/NavbarAdmin.vue')
+  }
+}
+</script>
 
 <style >
   
---
+++ client_clothing_shop/src/components/NavbarAdmin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="dark" style="background-color: var(--background-navbar) !important;">
+      <b-navbar-brand class="mx-auto">
+        <img src="../assets/image/logo.png" alt="logo k&I"  class="d-inline-block align-top logo selectable">
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarAdmin'
+}
+</script>
+
+<style scoped>
+  .logo {
+    width: 200px;
+  }
+
+</style>
