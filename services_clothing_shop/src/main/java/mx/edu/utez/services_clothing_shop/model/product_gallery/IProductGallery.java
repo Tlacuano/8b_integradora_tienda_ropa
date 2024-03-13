7ba3a2e3246d9cb00c24a138b941bcd4dac313af
FREECS33 <20213tn116@utez.edu.mx>
2024-03-13 12:41:14 -0600

    [UPDATE] Products - postProductGallery: Changed the method for insert the gallery of the product and implemented an method for delete the gallery in case of an error has ocurred

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product_gallery/IProductGallery.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/model/product_gallery/IProductGallery.java
@@ -1,13 +1,22 @@
 package mx.edu.utez.services_clothing_shop.model.product_gallery;
 
+import mx.edu.utez.services_clothing_shop.model.product.BeanProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IProductGallery extends JpaRepository<BeanProductGallery, UUID> {
-    @Query(value = "CALL sp_post_product_gallery(:idProduct, :idStatus: :image)", nativeQuery = true)
-    BeanProductGallery postProductGallery(String idProduct, String idStatus, String image);
+    List<BeanProductGallery> findByProduct(BeanProduct product);
+    @Query(value = "CALL sp_post_product_gallery(:p_id_product, :p_image, :p_id_status);", nativeQuery = true)
+    public void postProductGallery(
+            @Param("p_id_product") String idProduct,
+            @Param("p_image") String image,
+            @Param("p_id_status") String idStatus
+    );
+    public void deleteAllByProduct(BeanProduct product);
 }
