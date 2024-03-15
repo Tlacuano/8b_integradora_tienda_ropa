28bca3534250c4c7e815a2acfd8ef8ea02e6803e
DMarjar <20213tn119@utez.edu.mx>
2024-03-15 01:56:27 -0600

    [UPDATE] Order has products - getOrdersHasProductsByOrderId: implemented use of custom response

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/order_has_products/OrderHasProductsController.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/order_has_products/OrderHasProductsController.java
@@ -4,25 +4,26 @@ import mx.edu.utez.services_clothing_shop.controller.order_has_products.dto.Requ
 import mx.edu.utez.services_clothing_shop.controller.order_has_products.dto.ResponseOrderHasProductsDTO;
 import mx.edu.utez.services_clothing_shop.model.order_has_products.BeanOrderHasProducts;
 import mx.edu.utez.services_clothing_shop.service.order_has_products.OrderHasProductsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import mx.edu.utez.services_clothing_shop.utils.CustomResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("venta-ropa/api/order-has-products")
 @CrossOrigin(origins = "*")
 public class OrderHasProductsController {
-    @Autowired
-    private OrderHasProductsService orderHasProductsService;
+    private final OrderHasProductsService orderHasProductsService;
+
+    public OrderHasProductsController(OrderHasProductsService orderHasProductsService) {
+        this.orderHasProductsService = orderHasProductsService;
+    }
 
     @PostMapping("/get-orders-has-products-by-order-id")
-    public ResponseEntity<Page<ResponseOrderHasProductsDTO>> getOrdersHasProductsByOrderId(@RequestBody RequestOrderHasProductsByOrderIdDTO requestBody) {
+    public ResponseEntity<CustomResponse<Page<ResponseOrderHasProductsDTO>>> getOrdersHasProductsByOrderId(@RequestBody RequestOrderHasProductsByOrderIdDTO requestBody) {
         Page<BeanOrderHasProducts> ordersHasProducts = orderHasProductsService.getOrdersHasProductsByOrder_IdOrder(requestBody.getIdOrder(), requestBody.getPage());
         Page<ResponseOrderHasProductsDTO> dtoPage = ordersHasProducts.map(orderHasProducts -> new ResponseOrderHasProductsDTO().toOrderHasProductsDTO(orderHasProducts));
-        return ResponseEntity.status(HttpStatus.OK).body(dtoPage);
+        return ResponseEntity.status(HttpStatus.OK).body(new CustomResponse<>(dtoPage, "Orders has products found", false, HttpStatus.OK.value()));
     }
 }
