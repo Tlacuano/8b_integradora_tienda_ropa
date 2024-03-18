76e8776a9bf52f6e4e9e69d2bd9dd3d96ca07602
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-03-18 15:16:52 -0600

    [ADD] users - getPageUsers : complete de function to see de users

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/email_service/EmailService.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/service/email_service/EmailService.java
@@ -2,7 +2,6 @@ package mx.edu.utez.services_clothing_shop.service.email_service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import mx.edu.utez.services_clothing_shop.utils.CustomResponse;
 import mx.edu.utez.services_clothing_shop.utils.exception.CustomException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
