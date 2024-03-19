7339b1dec816c246fdd85c88a13d2824db5e8bed
Kellguzdi <20213tn114@utez.edu.mx>
2024-03-18 19:45:18 -0600

    [ADD] ShoppingCart - DTO to PUT:
    add a new DTO to view Shopping cart put information

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/shopping_cart/dto/ResponseShoppingCartDTO.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/shopping_cart/dto/ResponseShoppingCartDTO.java
@@ -4,13 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import mx.edu.utez.services_clothing_shop.controller.product.dto.ResponseShoppingCartProductDTO;
-import mx.edu.utez.services_clothing_shop.controller.user.dto.ResponseShoppingCartUserDTO;
 import mx.edu.utez.services_clothing_shop.model.shopping_cart.BeanShoppingCart;
 
 import java.util.UUID;
 
 @Data
-public class GetShoppingCartDTO {
+public class ResponseShoppingCartDTO {
     @NotNull
     private UUID idShoppingCart;
     @NotBlank(message = "shoppingCart.amount.notnull")
@@ -19,8 +18,8 @@ public class GetShoppingCartDTO {
     private ResponseShoppingCartProductDTO product;
 
 
-    public static GetShoppingCartDTO fromShoppingCart(BeanShoppingCart shoppingCart) {
-        GetShoppingCartDTO shoppingCartDTO = new GetShoppingCartDTO();
+    public static ResponseShoppingCartDTO fromShoppingCart(BeanShoppingCart shoppingCart) {
+        ResponseShoppingCartDTO shoppingCartDTO = new ResponseShoppingCartDTO();
         shoppingCartDTO.setIdShoppingCart(shoppingCart.getIdShoppingCart());
         shoppingCartDTO.setAmount(shoppingCart.getAmount());
         shoppingCartDTO.setProduct(ResponseShoppingCartProductDTO.fromProduct(shoppingCart.getProduct()));
