a71e53c6fe489763b80851bfe82a863e2d52597e
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2024-03-20 21:22:13 -0600

    [FIX] requests-data-change - Controller, Service: Some functions about the structure of the crud
    The endpoints were changed, as well as the way errors are handled. Access to the SpringSecurity endpoints was also modified.

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/requests_data_change/RequestsDataChangeController.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/requests_data_change/RequestsDataChangeController.java
@@ -10,8 +10,10 @@ import mx.edu.utez.services_clothing_shop.model.request_data_change.IRequestsDat
 import mx.edu.utez.services_clothing_shop.model.request_status.IRequestStatus;
 import mx.edu.utez.services_clothing_shop.service.requests_data_change.RequestsDataChangeService;
 import mx.edu.utez.services_clothing_shop.utils.CustomResponse;
-import mx.edu.utez.services_clothing_shop.utils.validations.RegexPatterns;
-import org.springframework.asm.TypeReference;
+
+import mx.edu.utez.services_clothing_shop.utils.exception.CustomException;
+
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -27,50 +29,47 @@ import java.util.*;
 public class RequestsDataChangeController {
 
     private final RequestsDataChangeService requestsDataChangeService;
-    private final IRequestsDataChange IRequestsDataChange;
 
-    public RequestsDataChangeController(RequestsDataChangeService requestsDataChangeService, IRequestsDataChange IRequestsDataChange) {
+    public RequestsDataChangeController(RequestsDataChangeService requestsDataChangeService) {
         this.requestsDataChangeService = requestsDataChangeService;
-        this.IRequestsDataChange = IRequestsDataChange;
     }
 
-    @PostMapping("/post")
-    public ResponseEntity<CustomResponse<String>> postRequest(@RequestBody RequestChangeInformationRequest request) {
+    @PostMapping("/post-request-data-change")
+    public ResponseEntity<CustomResponse<String>> postRequestDataChange(@RequestBody RequestChangeInformationRequest request) {
         String email = request.getEmail();
         NewInformation newInformation = request.getNewInformation();
-        requestsDataChangeService.postRequest(email, newInformation);
+        requestsDataChangeService.postRequestDataChange(email, newInformation);
         return ResponseEntity.ok(new CustomResponse<>("Request created successfully", "Request created", false, 200));
     }
 
-    @PutMapping("/put")
-    public ResponseEntity<CustomResponse<String>> updateRequestDataChange(@RequestBody RequestDataChangePutDTO requestData) {
+    @PutMapping("/put-request-data-change")
+    public ResponseEntity<CustomResponse<String>> putRequestDataChange(@RequestBody RequestDataChangePutDTO requestData) {
         UUID requestId = requestData.getIdRequestDataChange();
         String status = requestData.getStatus();
         String rejectionReason = requestData.getRejectionReason();
 
         try {
-            requestsDataChangeService.updateRequestDataChange(requestId, status, rejectionReason);
+            requestsDataChangeService.putRequestDataChange(requestId, status, rejectionReason);
             return ResponseEntity.ok(new CustomResponse<>("Request updated successfully", "Request updated", false, 200));
-        } catch (RequestsDataChangeService.InvalidRequestException e) {
+        } catch (CustomException e) {
             return ResponseEntity.badRequest().body(new CustomResponse<>(e.getMessage(), "Bad request", true, 400));
         }
     }
 
-    @GetMapping("/get-all")
-    public Page<String> getAllRequests(Pageable pageable) {
-        return IRequestsDataChange.findAllStatusNames(pageable);
+    @GetMapping("/get-page")
+    public Page<IRequestsDataChange.RequestDataChangeStatusProjection> getPageRequestDataChange(Pageable pageable) {
+        return requestsDataChangeService.getPageRequestDataChange(pageable);
     }
 
-    @PostMapping("/get-by-id")
-    public ResponseEntity<RequestDataChangeIdDTO> getRequestByID(@RequestBody RequestDataChangeIdDTO requestBody) {
+    @PostMapping("/get-by-id-request-data-change")
+    public ResponseEntity<RequestDataChangeIdDTO> getRequestDataChangeById(@RequestBody RequestDataChangeIdDTO requestBody) {
         UUID requestId = requestBody.getRequestId();
 
         try {
             RequestDataChangeIdDTO response = requestsDataChangeService.getRequestByID(requestId);
             return ResponseEntity.ok(response);
-        } catch (RequestsDataChangeService.NotFoundException e) {
+        } catch (CustomException e) {
             return ResponseEntity.notFound().build();
         }
     }
 }
-
