333099bdbeae2f11f10b4c7f068d910b069980d6
Kellguzdi <20213tn114@utez.edu.mx>
2024-03-25 02:09:23 -0600

    [ADD] ProductSalesRequests - modal: add modal to review request details

---
+++ client_clothing_shop/src/components/modals/ProductSalesRequestsDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-modal>
+    <h1>Detalles del producto</h1>
+  </b-modal>
+</template>
+<script>
+export default {
+  name: "ProductSalesRequestsDetails",
+  data() {
+
+  },
+
+}
+</script>
+<style scoped>
+
+</style>
