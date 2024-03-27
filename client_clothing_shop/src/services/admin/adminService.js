5346e5a367a0bf57b563e891520b2fd4b89c8e53
Kellguzdi <20213tn114@utez.edu.mx>
2024-03-26 23:20:29 -0600

    [UPDATE] productSalesRequests - function to axios: add new functions to axios

--- client_clothing_shop/src/services/admin/adminService.js
+++ client_clothing_shop/src/services/admin/adminService.js
@@ -1,25 +1,32 @@
 import axios from "../../config/http-client.gateway"
 
-const getPageProductSalesRequests = async (pagination)=>{
-    try{
-        const { page, size } = pagination;
+const getPageProductSalesRequests = async (pagination) => {
+    try {
+        const {page, size} = pagination;
         const response = await axios.doGet(`/venta-ropa/api/requests-sell-product/get-page?size=${size}&page=${page - 1}`);
-        console.log(response.data)
         return response.data
-    }catch (e){
+    } catch (e) {
         console.log(e)
     }
 }
-const getByIdProductSalesRequest = async (id)=>{
-    try{
-        const response = await axios.doPost(`/venta-ropa/api/requests-sell-product/get-by-id-request-sell-product`, {id});
-        console.log(response.data)
+const getByIdProductSalesRequest = async (id) => {
+    try {
+        const response = await axios.doPost('/venta-ropa/api/requests-sell-product/get-by-id-request-sell-product', {idRequestSellProduct: id})
         return response.data
-    }catch (e) {
+    } catch (e) {
         console.log(e)
     }
 }
-export default{
+const putProductSalesRequestStatus = async (id,status,rejectionReason) => {
+    try {
+        const response = await axios.doPut('/venta-ropa/api/requests-sell-product/put-request-sell-product', {idRequestSellProduct: id, status:status,rejectionReason:rejectionReason})
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export default {
     getPageProductSalesRequests,
-    getByIdProductSalesRequest
+    getByIdProductSalesRequest,
+    putProductSalesRequestStatus,
 };
