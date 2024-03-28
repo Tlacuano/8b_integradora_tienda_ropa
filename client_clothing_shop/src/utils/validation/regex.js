f0d762d7649945a67a5b0ca842fdb7c86171aa73
Kellguzdi <20213tn114@utez.edu.mx>
2024-03-27 22:26:27 -0600

    [UPDATE] ProductSalesRequest - regex: add new regex to rejection reason

--- client_clothing_shop/src/utils/validation/regex.js
+++ client_clothing_shop/src/utils/validation/regex.js
@@ -1,3 +1,4 @@
 export const regexName = /^([A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ]+[\s]?)+$/
 export const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
 export const regexCurp = /^([A-Z0-9]){18}$/
+export const regexRejectionReason = /^[a-zA-Z0-9 ]*$/
