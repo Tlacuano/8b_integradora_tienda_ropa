f6ecff42750469d43857c0bb43331a0ef1d5d194
DMarjar <20213tn119@utez.edu.mx>
2024-04-01 18:29:30 -0600

    [FIX] orders - mantainability: removed empty methods for annotations

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/order_has_products/dto/RequestOrderHasProductsByOrderIdDTO.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/order_has_products/dto/RequestOrderHasProductsByOrderIdDTO.java
@@ -1,16 +1,14 @@
 package mx.edu.utez.services_clothing_shop.controller.order_has_products.dto;
 
 import lombok.Data;
-import org.springframework.data.domain.Pageable;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 public class RequestOrderHasProductsByOrderIdDTO {
     private UUID idOrder;
-
-    public RequestOrderHasProductsByOrderIdDTO() {
-    }
 
     public RequestOrderHasProductsByOrderIdDTO(UUID idOrder) {
         this.idOrder = idOrder;
