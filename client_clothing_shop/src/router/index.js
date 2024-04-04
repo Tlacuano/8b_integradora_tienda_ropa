9ec658f6e54cf4dbd528f2cdc1092cbfa5e07623
Kellguzdi <20213tn114@utez.edu.mx>
2024-04-04 16:29:59 -0600

    [UPDATE] ProductManagement - props: send idProduct by props, and navigate in views to ProductManagement

--- client_clothing_shop/src/router/index.js
+++ client_clothing_shop/src/router/index.js
@@ -118,6 +118,7 @@ const router = new VueRouter({
                     name: "ProductDetails",
                     component: () => import("../views/product-management/ViewProductDetails.vue"),
                     meta: { requiresAuth: true, roles: ["SELLER"] },
+                    props:true
                 },
                 {
                     path:"product-sale",
@@ -136,11 +137,13 @@ const router = new VueRouter({
                     name:"RegisterProductEditionRequest",
                     component: () => import("../views/product-management/RegisterProductEditionRequest.vue"),
                     meta: { requiresAuth: true, roles: ["SELLER"] },
+                    props:true
                 },
                 {
                     path:"register-product-request",
                     name:"RegisterProductRequest",
                     component: () => import("../views/product-management/RegisterProductRequest.vue"),
+                    meta: { requiresAuth: true, roles: ["SELLER"] },
                 },
                 {
                     path:"request-data-change-management",
--- client_clothing_shop/src/views/product-management/ProductManagement.vue
+++ client_clothing_shop/src/views/product-management/ProductManagement.vue
@@ -18,59 +18,63 @@
         </b-form-group>
       </b-col>
       <b-col cols="auto" class="text-right">
-        <b-button variant="dark">Registrar</b-button>
+        <b-button variant="dark" @click="registerProductRequest">Registrar</b-button>
       </b-col>
     </b-row>
     <b-row class="container-products">
-      <b-col class="container-card mt-3" md="6" lg="4" sm="12" v-for="item in items" :key="item">
-        <b-card no-body class="card highlight-on-hover">
-          <b-row no-gutters class="body-card">
-            <b-col cols="12" sm="4" md="4" lg="4" class="image-container">
-              <b-img :src="item.productGallery[0].image"  class="img-fluid"></b-img>
-            </b-col>
-            <b-col cols="11" sm="6" md="6" lg="6" class="body-text">
-              <b-row>
-                <b-col>
-                  <h4>{{item.productName}}</h4>
+      <b-col>
+        <b-row>
+          <b-col class="container-card mt-3" md="6" lg="4" sm="12" v-for="item in items" :key="item.idProduct">
+            <b-card no-body class="card highlight-on-hover">
+              <b-row no-gutters class="body-card">
+                <b-col cols="12" sm="4" md="4" lg="4" class="image-container">
+                  <b-img :src="item.productGallery[0].image"  class="img-fluid"></b-img>
+                </b-col>
+                <b-col cols="11" sm="6" md="6" lg="6" class="body-text">
+                  <b-row>
+                    <b-col>
+                      <h4 class="mb-0">{{item.productName}}</h4>
+                    </b-col>
+                  </b-row>
+                  <b-row class="mt-2">
+                    <b-col >
+                      <strong>Categoria: <p style="display: inline">{{item.category}}</p></strong>
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col>
+                      <strong>Subcategoria: <p style="display: inline">{{item.subcategory}}</p></strong>
+                    </b-col>
+                  </b-row>
+                  <b-row class="mt-3">
+                    <b-col cols="9" sm="10" md="8" lg="9" >
+                      <p>Precio <strong style="display: inline">${{item.price}}</strong></p>
+                    </b-col>
+                    <b-col cols="3" sm="2" md="4" lg="3">
+                      <b-badge :variant="item.status ? 'success' : 'danger'">
+                        {{ item.status ? 'Habilitado' : 'Deshabilitado' }}
+                      </b-badge>
+                    </b-col>
+                  </b-row>
+                </b-col>
+                <b-col cols="1" sm="1" md="1" lg="1" class="mt-2 dropdown">
+                  <b-dropdown
+                      variant="link-dark"
+                      toggle-class="text-decoration-none"
+                      no-caret
+                  >
+                    <template v-slot:button-content>
+                      <font-awesome-icon icon="ellipsis-v"/>
+                    </template>
+                    <b-dropdown-item @click="viewProductDetail(item.idProduct)">Ver detalles</b-dropdown-item>
+                    <b-dropdown-item @click="registerProductEditionRequest(item.idProduct)">Editar</b-dropdown-item>
+                    <b-dropdown-item @click="putStatusProduct(item.idProduct)">{{item.status ? 'Deshabilitar' : 'Habilitar'}}</b-dropdown-item>
+                  </b-dropdown>
                 </b-col>
               </b-row>
-              <b-row class="mt-3">
-                <b-col >
-                  <strong>Categoria: <p style="display: inline">{{item.category}}</p></strong>
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col>
-                  <strong>Subcategoria: <p style="display: inline">{{item.subcategory}}</p></strong>
-                </b-col>
-              </b-row>
-              <b-row class="mt-5">
-                <b-col cols="9" sm="10" md="10" lg="9" >
-                  <p>Precio <strong style="display: inline">${{item.price}}</strong></p>
-                </b-col>
-                <b-col cols="3" sm="2" md="2" lg="3">
-                  <b-badge :variant="item.status ? 'success' : 'danger'">
-                    {{ item.status ? 'Habilitado' : 'Deshabilitado' }}
-                  </b-badge>
-                </b-col>
-              </b-row>
-            </b-col>
-            <b-col cols="1" sm="1" md="1" lg="1" class="mt-2 dropdown">
-              <b-dropdown
-                  variant="link-dark"
-                  toggle-class="text-decoration-none"
-                  no-caret
-              >
-                <template v-slot:button-content>
-                  <font-awesome-icon icon="ellipsis-v"/>
-                </template>
-                <b-dropdown-item>Ver detalles</b-dropdown-item>
-                <b-dropdown-item>Editar</b-dropdown-item>
-                <b-dropdown-item>Desabilitar</b-dropdown-item>
-              </b-dropdown>
-            </b-col>
-          </b-row>
-        </b-card>
+            </b-card>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
     <b-row>
@@ -88,9 +92,16 @@
 <script>
 
   import ProductManagementService from "@/services/product-management/ProductManagementService";
+  import ProductDetails from "@/views/product/ProductDetails.vue";
+  import {showSuccessToast, showWarningToast} from "@/components/alerts/alerts";
 
   export default {
     name: "ProductManagement",
+    computed: {
+      ProductDetails() {
+        return ProductDetails
+      }
+    },
     data() {
       return {
         objectPagination:{
@@ -106,7 +117,31 @@
         const email = this.$store.getters.getEmail
         const response = await ProductManagementService.getProductByUser(this.objectPagination,email)
         this.items = response.data.content
+        console.log(response.data.content)
         this.objectPagination.elements = response.totalElements
+      },
+      async putStatusProduct(idProduct){
+        const response = await ProductManagementService.putStatusProduct(idProduct)
+        if(response){
+          showSuccessToast("Realizado con éxito", "El estado del product se actualizo correctamente")
+          this.getProductByUser()
+              .then(product => {
+                console.log("Éxito");
+              })
+              .catch(error => {
+              });
+        }else{
+          showWarningToast("Ocurrio un error", "La actualización del estado fallo")
+        }
+      },
+      viewProductDetail(idProduct){
+        this.$router.push({name:'ProductDetails',params:{idProduct:idProduct}})
+      },
+      registerProductEditionRequest(idProduct){
+        this.$router.push({name:'RegisterProductEditionRequest',params:{idProduct:idProduct}})
+      },
+      registerProductRequest(){
+        this.$router.push({name:'RegisterProductRequest'})
       }
     },
     mounted() {
@@ -119,9 +154,6 @@
 .container-products {
   height: calc(100vh - 270px);
   overflow-x: hidden;
-}
-.card{
-  height: 195px;
 }
 .body-card{
   height: 100%;
@@ -162,9 +194,6 @@
   .body-text {
     margin-left: 10px;
     margin-top: 20px;
-  }
-  .card{
-    height: 450px;
   }
   .dropdown{
     margin-left:-10px
