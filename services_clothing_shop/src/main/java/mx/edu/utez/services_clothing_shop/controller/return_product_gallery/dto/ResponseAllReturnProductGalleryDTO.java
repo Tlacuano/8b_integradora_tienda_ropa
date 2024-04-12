434a92ba219081e888ecb2cfed03be8e95290697
DMarjar <20213tn119@utez.edu.mx>
2024-04-12 00:06:40 -0600

    [FIX] backend quality - ResponseAllReturnProductGalleryDTO: added annotation no args constructor

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/return_product_gallery/dto/ResponseAllReturnProductGalleryDTO.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/return_product_gallery/dto/ResponseAllReturnProductGalleryDTO.java
@@ -1,15 +1,15 @@
 package mx.edu.utez.services_clothing_shop.controller.return_product_gallery.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseAllReturnProductGalleryDTO {
     private String image;
     private UUID requestReturnProductId;
-
-    public ResponseAllReturnProductGalleryDTO() {
-    }
-
 }
