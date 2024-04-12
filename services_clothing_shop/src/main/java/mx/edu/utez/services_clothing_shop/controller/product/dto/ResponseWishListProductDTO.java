2348afe15062df55e7af0fcf45f95aba25890306
DMarjar <20213tn119@utez.edu.mx>
2024-04-11 23:57:27 -0600

    [FIX] backend quality - ResponseWishlistProductDTO: added annotation no args constructor

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/product/dto/ResponseWishListProductDTO.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/product/dto/ResponseWishListProductDTO.java
@@ -1,22 +1,22 @@
 package mx.edu.utez.services_clothing_shop.controller.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import mx.edu.utez.services_clothing_shop.model.product.BeanProduct;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseWishListProductDTO {
     private UUID idProduct;
     private String productName;
     private double price;
     private int amount;
 
-
-    public ResponseWishListProductDTO(){
-
-    }
-    public static ResponseWishListProductDTO fromProduct (BeanProduct product){
+    public static ResponseWishListProductDTO fromProduct(BeanProduct product) {
         ResponseWishListProductDTO productDTO = new ResponseWishListProductDTO();
         productDTO.setIdProduct(product.getIdProduct());
         productDTO.setProductName(product.getProductName());
