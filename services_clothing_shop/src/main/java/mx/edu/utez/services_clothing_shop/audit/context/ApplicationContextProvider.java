ab31dda7f0e265c37cd85311f23f733d0c08dceb
DMarjar <20213tn119@utez.edu.mx>
2024-04-12 15:06:39 -0600

    [FIX] backend quality - applicaton context: returned original application context with sonarqube issues

---
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/audit/context/ApplicationContextProvider.java
@@ -0,0 +1,19 @@
+package mx.edu.utez.services_clothing_shop.audit.context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        context = applicationContext;
+    }
+}
