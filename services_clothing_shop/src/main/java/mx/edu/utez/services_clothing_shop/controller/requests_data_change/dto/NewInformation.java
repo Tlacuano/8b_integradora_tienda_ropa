9facf29009cb39acf1320e54ddb31ae2077a913a
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2024-04-13 02:33:56 -0600

    [FIX] management-requests-data-change - Get page. Get by id, Put: Now the information of the functionts its about the SELLER AND NOT THE PERSON
    Why? From the beginning, they didn't tell me it was information about a SELLER, not a PERSON, which is what comes to mind when you hear about NORMAL information. Anyway, tomorrow I'll create the modal for the request because I realized there is no service that retrieves the seller's information through email, and well... I'll have to implement it now to finish quickly.

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/requests_data_change/dto/NewInformation.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/controller/requests_data_change/dto/NewInformation.java
@@ -1,37 +1,16 @@
 package mx.edu.utez.services_clothing_shop.controller.requests_data_change.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewInformation {
-    private String name;
-    private String lastName;
-    private String secondLastName;
-    private String birthday;
-    private String phoneNumber;
-    private String gender;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getSecondLastName() {
-        return secondLastName;
-    }
-
-
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
+    private String taxIdentificationNumber;
+    private String secondaryPhoneNumber;
+    private String imageIdentification;
+    private String curp;
 }
