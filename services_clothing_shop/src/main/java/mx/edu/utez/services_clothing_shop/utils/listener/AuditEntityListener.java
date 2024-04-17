8e42fd21ef964841282c83c9364c3a7e2db44055
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-04-17 17:53:34 -0600

    [ADD] users- oldvalue : to see the oldvlaue in the table controle

--- services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/utils/listener/AuditEntityListener.java
+++ services_clothing_shop/src/main/java/mx/edu/utez/services_clothing_shop/utils/listener/AuditEntityListener.java
@@ -3,6 +3,7 @@ package mx.edu.utez.services_clothing_shop.utils.listener;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostRemove;
 import jakarta.persistence.PostUpdate;
+import jakarta.persistence.PreUpdate;
 import jakarta.servlet.http.HttpServletRequest;
 import mx.edu.utez.services_clothing_shop.audit.context.ApplicationContextProvider;
 import mx.edu.utez.services_clothing_shop.audit.model.AuditLog;
@@ -15,27 +16,40 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class AuditEntityListener {
 
+
+    private final ThreadLocal<String> oldValue = new ThreadLocal<>();
+
+    @PreUpdate
+    private void beforeUpdate(Object target) {
+        String oldValue = Convert.toJSON(target);
+        String oldValueEmbeddedRemoved = Convert.removeEmbedded(oldValue);
+
+        this.oldValue.set(oldValueEmbeddedRemoved);
+    }
+
     @PostPersist
     private void afterCreate(Object target) {
         String newValue = Convert.toJSON(target);
 
-        recordAuditAction(target, "CREATE", newValue);
+        recordAuditAction(target, "CREATE", newValue, null);
     }
 
     @PostRemove
     private void afterDelete(Object target) {
-        recordAuditAction(target, "DELETE", null);
+        recordAuditAction(target, "DELETE", null, null);
     }
 
     @PostUpdate
-    private void beforeUpdate(Object target) {
-
+    private void postUpdate(Object target) {
         String newValue = Convert.toJSON(target);
+        String oldVal = oldValue.get();
 
-        recordAuditAction(target, "UPDATE", newValue);
+        recordAuditAction(target, "UPDATE",  newValue, oldVal);
+
+        oldValue.remove();
     }
 
-    private void recordAuditAction(Object target, String action, String newValue) {
+    private void recordAuditAction(Object target, String action, String newValue, String oldValue) {
         // Get the user and IP address from the SecurityContextAspect
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes != null ? attributes.getRequest() : null;
@@ -51,11 +65,12 @@ public class AuditEntityListener {
         auditLog.setAction(action);
         auditLog.setTableName(target.getClass().getSimpleName());
 
-        // Encrypt the values
+        // set Velues
+
         String newValueCode = newValue != null ? Convert.toJSON(newValue) : newValue;
         String newValueEmbeddedRemoved = Convert.removeEmbedded(newValueCode);
 
-
+        auditLog.setOldValue(oldValue);
         auditLog.setNewValue(newValueEmbeddedRemoved);
 
 
